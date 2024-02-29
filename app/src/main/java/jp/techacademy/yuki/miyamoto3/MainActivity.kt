1cb4e03d68f957f312c9fd5e27e85bc966cdc48f
miya <>
2024-02-29 18:51:12 +0900

    最初のコミット

---
+++ app/src/main/java/jp/techacademy/yuki/miyamoto3/MainActivity.kt
@@ -0,0 +1,11 @@
+package jp.techacademy.yuki.miyamoto3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
